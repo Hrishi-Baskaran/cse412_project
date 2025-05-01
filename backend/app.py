0ff29065a797596dc0a1d81dd3f2fb69d5aaf79d
Hrishi <hthevar@asu.edu>
2025-04-30 19:46:13 -0700

    Adds backend for cites relation

--- backend/app.py
+++ backend/app.py
@@ -50,13 +50,13 @@ def handle_organization():
 
                 params = []
                 conditions = []
-                org_name = request.form.get('organization_name', None)
+                paper_id = request.form.get('organization_name', None)
                 profile = request.form.get('profile', None)
                 org_card = request.form.get('organization_card', None)
                 
-                if org_name is not None:
+                if paper_id is not None:
                     conditions.append('organization_name = %s')
-                    params.append(org_name)
+                    params.append(paper_id)
                 if profile is not None:
                     conditions.append('profile = %s')
                     params.append(profile)
@@ -78,11 +78,11 @@ def handle_organization():
                 params = []
                 update_statements = []
 
-                org_name = request.form.get('organization_name', None)
+                paper_id = request.form.get('organization_name', None)
                 profile = request.form.get('profile', None)
                 org_card = request.form.get('organization_card', None)
 
-                if org_name == None:
+                if paper_id == None:
                     return "Name of organization to update not specified", 400
                 
                 if profile is not None:
@@ -115,7 +115,62 @@ def handle_organization():
         return f"Error: {e}"
             
 
+@app.route('/cites', methods=['GET', 'POST', 'DELETE', 'PUT'])
+def handle_cites():
+    try:
+        cur = conn.cursor()
 
+        match request.method:
+            case 'GET':
+                cur.execute('SELECT * FROM cites;')
+                citations = cur.fetchall()
+
+                result = []
+
+                for cite in citations:
+                    result.append({
+                        'paper_id': cite[0],
+                        'model_id': cite[1]
+                    })
+                
+                return jsonify(result), 200
+
+            case 'POST':
+                cur.execute(f"INSERT INTO cites VALUES ('{request.form['paper_id']}', '{request.form['model_id']}')")
+                conn.commit()
+
+                return 201
+            case 'DELETE':
+
+                params = []
+                conditions = []
+                paper_id = request.form.get('paper_id', None)
+                model_id = request.form.get('model_id', None)
+                
+                if paper_id is not None:
+                    conditions.append('paper_id = %s')
+                    params.append(paper_id)
+                if model_id is not None:
+                    conditions.append('model_id = %s')
+                    params.append(model_id)
+
+                query = "DELETE FROM cites WHERE " + " AND ".join(conditions)
+
+                if (len(params) > 0):
+                    cur.execute(query, tuple(params))
+                    cnt = cur.rowcount
+                    conn.commit()
+                    return f"Deleted {cnt} citations", 200
+                else:
+                    return "No deletion criterion provided", 400
+                
+
+
+                
+                
+
+    except Exception as e:
+        return f"Error: {e}"
 
 if __name__ == '__main__':
     app.run(debug=True)
